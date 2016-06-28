62b09be4a2b788dec4a111c15a31a24daf07d248
Maryadi <maryadi@elephanttalk.com>
2016-06-28 20:20:27 +0700

    setting up...

---
+++ app/src/main/java/corp/app/simplicity/com/maryadi_chess/MainActivity.java
@@ -0,0 +1,13 @@
+package corp.app.simplicity.com.maryadi_chess;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
